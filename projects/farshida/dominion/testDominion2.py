19809dd62fa399e5a3ac8fcecf343585ff3ddbbd
Anousha <anousha.kf@gmail.com>
2020-01-17 23:22:43 -0800

    message

---
+++ projects/farshida/dominion/testDominion2.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 15 2020
+
+@author: Anousha Farshid
+"""
+
+import Dominion
+
+import testUtility
+import random
+from collections import defaultdict
+
+# Get player names
+player_names = testUtility.getNames()
+
+# number of curses and victory cards
+if len(player_names) > 2:
+    nV = 12
+else:
+    nV = 8
+nC = -10 + 10 * len(player_names)
+
+# Define box
+
+box = testUtility.getBoxes(nV)
+
+
+supply_order = testUtility.getSupplyOrder(nC)
+
+# Pick 10 cards from box to be in the supply.
+boxlist = [k for k in box]
+random.shuffle(boxlist)
+random10 = boxlist[:10]
+
+supply = testUtility.getAlwaysSupply()
+
+
+# initialize the trash
+trash = []
+
+# Construct the Player objects
+players = []
+for name in player_names:
+    if name[0] == "*":
+        players.append(Dominion.ComputerPlayer(name[1:]))
+    elif name[0] == "^":
+        players.append(Dominion.TablePlayer(name[1:]))
+    else:
+        players.append(Dominion.Player(name))
+
+# Play the game
+turn = 0
+while not Dominion.gameover(supply):
+    turn += 2        #test Scnario
+    print("\r")
+    for value in supply_order:
+        print (value)
+        for stack in supply_order[value]:
+            if stack in supply:
+                print (stack, len(supply[stack]))
+    print("\r")
+    for player in players:
+        print (player.name, player.calcpoints())
+    print ("\rStart of turn " + str(turn))
+    for player in players:
+        if not Dominion.gameover(supply):
+            print("\r")
+            player.turn(players, supply, trash)
+
+# Final score
+dcs = Dominion.cardsummaries(players)
+vp = dcs.loc['VICTORY POINTS']
+vpmax = vp.max()
+winners = []
+for i in vp.index:
+    if vp.loc[i] == vpmax:
+        winners.append(i)
+if len(winners) > 1:
+    winstring = ' and '.join(winners) + ' win!'
+else:
+    winstring = ' '.join([winners[0], 'wins!'])
+
+print("\nGAME OVER!!!\n" + winstring + "\n")
+print(dcs)
